a64f9a0e85d4332c575c3e71c1c999956b95cb13
ag3ntclov3r <127633281+404humornotfound@users.noreply.github.com>
2025-07-27 23:45:55 -0700

    im tired boss

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # comic-archive-scraper
-I just want to read comics, and this is a tool for getting one that doesn't really exist on the internet
+I just want to read comics, and this is a tool for getting one that doesn't really exist on the internet and I wanted to read locally
--- main.py
+++ main.py
@@ -1,11 +1,45 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from zipfile import ZipFile
+import os
+from os.path import basename
+from functions import *
+import time
 url = "https://webcomicarchive.com/#/WebToons/PinchPoint?strip=1-000.jpg"
-result = requests.get(url).text
-soup = bs(result, features="html.parser")
+# result = requests.get(url).text
+# soup = bs(result, features="html.parser")
+"""
+pure jpg: 
+https://webcomicarchive.com/comic/WebToons/PinchPoint/1-000.jpg
 
-tags = soup.find_all("list-group list-group-flush")
-print(tags)
-# https://webcomicarchive.com/comic/WebToons/PinchPoint/1-001.jpg
+big html:
+https://webcomicarchive.com/#/WebToons/PinchPoint?strip=1-000.jpg
 
-# class = list-group list-group-flush  ... id = sidebarStrips
+
+plan: change # to comic and change ?strip= to /
+
+
+"""
+# start("https://webcomicarchive.com/#/WebToons/PinchPoint?strip=1-000.jpg")
+
+
+with open("w.html", "r", encoding="utf8") as f:
+    soup = bs(f, "html.parser")
+
+tags = soup.find_all("a", class_ = "list-group-item block", href=True)
+tags.insert(0, soup.find_all("a", class_="list-group-item block border-top-0", href=True))
+
+
+i = tags[0][0]
+time.sleep(5)
+lnk = i.get('href')
+lnk = lnk.replace("#", "comic")
+lnk = lnk.replace("?strip=", "/")
+
+response = requests.get(lnk)
+if response.status_code == 200:
+    with open("a.jpg", "wb") as f:
+        f.write(response.content)
+else:
+    print(f"error: {response.status_code}")
+
